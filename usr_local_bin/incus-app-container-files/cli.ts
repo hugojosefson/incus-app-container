8ba64a68efeaa534b54880c3942403756c05ab13
Hugo Josefson <hugo@josefson.org>
2024-03-02 20:33:09 +0100

    feat: create container with ip

--- usr_local_bin/incus-app-container-files/cli.ts
+++ usr_local_bin/incus-app-container-files/cli.ts
@@ -1,4 +1,4 @@
-/**
+/*
  * via https://github.com/hugojosefson/incus-app-container
  * License: MIT
  * Copyright (c) 2022 Hugo Josefson
@@ -6,81 +6,168 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+import { breadc, ParseError } from "npm:breadc@0.9.7";
+import { s } from "https://deno.land/x/fns@1.1.0/string/s.ts";
+import { isString } from "https://deno.land/x/fns@1.1.0/string/is-string.ts";
+import { createAppContainer } from "./ct-template.ts";
 
-import {
-  createCt,
-  ensureExistsCtTemplate,
-  getShortName,
-} from "./ct-template.ts";
-import {
-  CONTENT_CT_ROOTDIR,
-  CONTENT_CT_TEMPLATE,
-  getStorage,
-} from "./storage.ts";
-import { VMID } from "./os.ts";
-
-/** template filenames we support. the first one is default if user does not specify. */
-const compatibleTemplates = [
-  "alpine-3.18-default_20230607_amd64.tar.xz",
-  "ubuntu-22.04-standard_22.04-1_amd64.tar.zst",
-  "debian-12-standard_12.0-1_amd64.tar.zst",
-];
-
-const defaultTemplate = compatibleTemplates[0];
-const [name, CT_BASE_TEMPLATE_FILENAME = defaultTemplate] = Deno.args;
-
-function usageAndExit(code = 2): never {
-  console.error(`USAGE:
-
-  incus-app-container --help                              This help message
-  incus-app-container <name>                              Create a CT from ${defaultTemplate}
-  incus-app-container <name> [<base_template_filename>]   Create a CT from specified base template
-
-EXAMPLE:
-
-  incus-app-container my-service
-  ${
-    compatibleTemplates.map((filename) =>
-      `incus-app-container my-service ${filename}`
-    ).join("\n  ")
+export type GetterOr<T> = T | (() => T);
+export type PromiseOr<T> = T | Promise<T>;
+export type AsyncGetterOr<T> = GetterOr<PromiseOr<T>>;
+export type ValidValues<T> = AsyncGetterOr<Readonly<T[] | Set<T> | string>>;
+async function resolveValidValueMessage<T>(
+  validValues: ValidValues<T>,
+): Promise<string | undefined> {
+  validValues = await validValues;
+  if (typeof validValues === "string") {
+    const message: string = validValues;
+    return message;
   }
-`);
-  Deno.exit(code);
+  if (typeof validValues === "function") {
+    return resolveValidValueMessage(validValues());
+  }
+  if (validValues instanceof Set) {
+    return `one of: ${[...validValues].join(", ")}.`;
+  }
+  if (Array.isArray(validValues)) {
+    return `one of: ${validValues.join(", ")}.`;
+  }
+  return undefined;
 }
 
-if (!name || name === "--help") {
-  usageAndExit();
+export const OUTPUT_FORMATS = ["table", "json", "jsonl", "plain"] as const;
+export type OutputFormat = typeof OUTPUT_FORMATS[number];
+export const isOutputFormat = (value: unknown): value is OutputFormat =>
+  OUTPUT_FORMATS.includes(value as OutputFormat);
+
+export type SshKeyUsernamePrefix = "gh" | "local";
+export type PrefixedUsername<Prefix extends SshKeyUsernamePrefix> =
+  `${Prefix}:${string}`;
+export const PREFIXED_USERNAME_REGEX = /^(gh|local):/;
+export type SshKeyRaw = `{string} {string}`;
+export type SshKey =
+  | PrefixedUsername<SshKeyUsernamePrefix>
+  | SshKeyRaw;
+
+export function isSshKeyRaw(value: unknown): value is SshKeyRaw {
+  if (!isString(value)) {
+    return false;
+  }
+  return value.includes(" ");
 }
 
-const DOCKER_CT_TEMPLATE_NAME = "docker-ct-" +
-  getShortName(CT_BASE_TEMPLATE_FILENAME);
-const DOCKER_CT_TEMPLATE_FILENAME = `docker-ct-${CT_BASE_TEMPLATE_FILENAME}`;
+export function isPrefixedUsername(
+  value: unknown,
+): value is PrefixedUsername<SshKeyUsernamePrefix> {
+  if (!isString(value)) {
+    return false;
+  }
+  return PREFIXED_USERNAME_REGEX.test(value);
+}
 
-const templateVmid: VMID = await ensureExistsCtTemplate({
-  baseTemplateStorage: () =>
-    getStorage(
-      CONTENT_CT_TEMPLATE,
-      CT_BASE_TEMPLATE_FILENAME,
-    ),
-  baseFilename: CT_BASE_TEMPLATE_FILENAME,
-  storage: () =>
-    getStorage(
-      CONTENT_CT_ROOTDIR,
-      DOCKER_CT_TEMPLATE_FILENAME,
-    ),
-  name: DOCKER_CT_TEMPLATE_NAME,
-  filename: DOCKER_CT_TEMPLATE_FILENAME,
+export function isPrefixedUsernameWith(prefix: SshKeyUsernamePrefix) {
+  return (value: unknown): value is PrefixedUsername<typeof prefix> =>
+    isPrefixedUsername(value) && value.startsWith(prefix + ":");
+}
+
+export function isSshKey(value: unknown): value is SshKey {
+  return isSshKeyRaw(value) || isPrefixedUsername(value);
+}
+
+export async function enforceType<T>(
+  typeGuard: (value: unknown) => value is T,
+  validValues: ValidValues<T> = [],
+  name = typeGuard.name.replace(/^is/, ""),
+): Promise<(value: unknown) => T> {
+  const validValueMessage = await resolveValidValueMessage(validValues);
+  return (value: unknown): T => {
+    if (!typeGuard(value)) {
+      const message = `Invalid ${name}: ${value}.` +
+        (validValueMessage ? `\nMust be ${validValueMessage}` : "");
+      throw new ParseError(message);
+    }
+    return value;
+  };
+}
+
+export function isArrayOf<T>(
+  typeGuard: (value: unknown) => value is T,
+): (value: unknown) => value is T[] {
+  return (value: unknown): value is T[] =>
+    Array.isArray(value) && value.every(typeGuard);
+}
+
+export function optional<T>(
+  typeGuard: (value: unknown) => value is T,
+): (value: unknown) => value is T | undefined {
+  return (value: unknown): value is T | undefined =>
+    value === undefined || typeGuard(value);
+}
+
+const cli = breadc("incus-app-container", {
+  description: "Opinionated script for creating Incus containers for apps.",
+  version: "0.0.0",
 });
 
-const { vmid, appdataDir } = await createCt({
-  templateVmid,
-  name,
-  storage: () =>
-    getStorage(
-      undefined,
-      `appdata for CT ${name}`,
-    ),
-});
+cli
+  .command("create <container_name>", "Create a new Incus app container.")
+  .option(
+    "--cidr <cidr>",
+    {
+      description:
+        "Network address for the container in CIDR format, for example 10.20.30.40/24; or 'dhcp'.",
+      default: "dhcp",
+    },
+  )
+  .option(
+    "--ssh-key <ssh-key>",
+    {
+      description:
+        "Public ssh key(s) to add to the container's authorized_keys file. Actual key, or `gh:${username}` for importing from GitHub, or `local:${username}` for importing from local user.",
+      cast: await enforceType(
+        optional(isSshKey),
+        "an actual ssh public key, or `gh:${username}` for importing from GitHub, or `local:${username}` for importing from local user.",
+        "ssh-key",
+      ),
+    },
+  )
+  .action(async (name: string, { cidr, sshKey }) => {
+    console.log(s(name));
+    const { appdataDir } = await createAppContainer({ name, cidr, sshKey });
+    console.log(appdataDir);
+  });
 
-console.log(vmid);
-console.log(appdataDir);
+cli
+  .command("delete <container_name>", "Delete an Incus app container.")
+  .action((containerName: string) => {
+    console.log(`Deleting container ${s(containerName)}`);
+  });
+
+cli
+  .command("list", "List all Incus app containers.")
+  .option("--format <format>", {
+    description: "Output format.",
+    default: "table",
+    cast: await enforceType(isOutputFormat, OUTPUT_FORMATS, "format"),
+  })
+  .action(({ format }) => {
+    console.log(`Listing containers as ${format}...`);
+  });
+
+try {
+  await cli.run(Deno.args);
+} catch (e) {
+  if (e instanceof ParseError) {
+    const hr = "-".repeat(Deno.consoleSize().columns);
+    await cli.run(["--help"]);
+    console.error(
+      [
+        hr,
+        e.message,
+        hr,
+      ].join("\n"),
+    );
+    Deno.exit(2);
+  }
+  throw e;
+}
--- usr_local_bin/incus-app-container-files/ct-template.ts
+++ usr_local_bin/incus-app-container-files/ct-template.ts
@@ -1,55 +1,14 @@
-import { getCtTemplate, PctEntry, VMID } from "./os.ts";
+import { isSshKeyRaw, SshKeyRaw } from "./cli.ts";
+import { AsyncGetterOr, isPrefixedUsername } from "./cli.ts";
+import { SshKey } from "./cli.ts";
 import { dirname, run } from "./deps.ts";
-import { CONTENT_CT_TEMPLATE, StorageRow } from "./storage.ts";
 import { readFromUrl } from "./read-from-url.ts";
-import { getNetworkInterface } from "./network.ts";
-import { extractShebangCommand, extractValueFromPveShGet } from "./fn.ts";
-
-/**
- * Extracts a short alphanumeric (plus dash) name from a ct base template filename, for example "ubuntu-2204", or "alpine-316".
- * @param filename
- */
-export function getShortName(filename: string): string {
-  return filename.split("-").slice(0, 2).join("-")
-    .replaceAll(/[^a-z0-9-]/g, "");
-}
-
-async function getAppdataDir(
-  storage: Pick<StorageRow, "name">,
-  options: Pick<CtTemplateOptions, "name">,
-) {
-  const path = extractValueFromPveShGet(
-    await run(
-      ["pvesh", "get", `/storage/${storage.name}`],
-    ),
-    [
-      "path",
-      "mountpoint",
-    ],
-  );
-  if (!path) {
-    throw new Error(
-      `Could not find path or mountpoint for storage ${storage.name}.`,
-    );
-  }
-
-  return `${path}/appdata/${options.name}`;
-}
-
-type CtTemplateOptions = {
-  baseTemplateStorage: () => Promise<StorageRow>;
-  baseFilename: string;
-  storage: () => Promise<StorageRow>;
-  name: string;
-  filename: string;
-  vmid?: VMID;
-  cores?: number;
-  memoryMegabytes?: number;
-};
+import { stringify as yaml } from "https://deno.land/std@0.218.2/yaml/stringify.ts";
+import IPCIDR from "npm:ip-cidr@4.0.0";
 
 async function readInstallScript(baseFilename: string): Promise<string> {
   const url = new URL(
-    `template/${getShortName(baseFilename)}-install`,
+    `template/${baseFilename}-install`,
     import.meta.url,
   );
   try {
@@ -66,136 +25,210 @@ Only install scripts in ${dirname(url.toString())}/ are supported.`,
   }
 }
 
-export async function createCtTemplate(
-  options: CtTemplateOptions,
-): Promise<VMID> {
-  const templateVolumeId = await ensureExistsTemplate(
-    options.baseTemplateStorage,
-    options.baseFilename,
-  );
-  const vmid: VMID = options.vmid ??
-    parseInt(await run("pvesh get /cluster/nextid"), 10);
-  const bridgeName = (await getNetworkInterface("bridge")).name;
-  const storageName = (await options.storage()).name;
-  const installScript = await readInstallScript(options.baseFilename);
-
-  await run([
-    "pct",
-    "create",
-    vmid,
-    templateVolumeId,
-    "--cores",
-    options.cores ?? 2,
-    "--description",
-    (await readFromUrl(new URL("template/summary.md", import.meta.url)))
-      .replaceAll("${APP_NAME}", options.name)
-      .replaceAll("${DOCKER_CT_TEMPLATE_FILENAME}", options.baseFilename),
-    "--features",
-    "nesting=1",
-    "--hostname",
-    options.name,
-    "--memory",
-    options.memoryMegabytes ?? 2048,
-    "--swap",
-    options.memoryMegabytes ?? 2048,
-    "--net0",
-    `name=eth0,bridge=${bridgeName},firewall=1,ip=dhcp`,
-    "--ostype",
-    options.baseFilename.split("-")[0],
-    "--ssh-public-keys",
-    "/root/.ssh/authorized_keys",
-    "--timezone",
-    "host",
-    "--template",
-    0,
-    "--storage",
-    storageName,
-    "--unprivileged",
-    1,
-  ]);
-  await run(["pct", "start", vmid]);
-  await run(
-    ["pct", "exec", vmid, "--", ...extractShebangCommand(installScript)],
-    {
-      stdin: installScript,
-    },
-  );
-  await run(["pct", "shutdown", vmid]);
-  await run(["pct", "template", vmid]);
-  return vmid;
+export async function resolveMultiArgument<T>(
+  multiArgument: MultiArgument<T>,
+): Promise<T[]> {
+  multiArgument = await multiArgument;
+  if (multiArgument === undefined) {
+    return [];
+  }
+  if (typeof multiArgument === "function") {
+    return resolveMultiArgument(multiArgument());
+  }
+  if (multiArgument instanceof Set) {
+    return resolveMultiArgument([...multiArgument]);
+  }
+  if (Array.isArray(multiArgument)) {
+    return await Promise.all(multiArgument.map(resolveMultiArgument)) as T[];
+  }
+  return [multiArgument] as T[];
 }
 
-type CtOptions = {
-  templateVmid: VMID;
-  vmid?: VMID;
-  name: string;
-  storage: () => Promise<StorageRow>;
+export type MultiArgument<T> = AsyncGetterOr<
+  Readonly<T[] | Set<T> | T> | undefined
+>;
+
+export type PasswdRow = {
+  username: string;
+  password: string;
+  uid: number;
+  gid: number;
+  gecos: string;
+  home: string;
+  shell: string;
 };
 
-export async function createCt(
-  options: CtOptions,
-): Promise<{ vmid: VMID; appdataDir: string }> {
-  const vmid: VMID = options.vmid ??
-    parseInt(await run("pvesh get /cluster/nextid"), 10);
-
-  const cloneCmd = [
-    "pct",
-    "clone",
-    options.templateVmid,
-    vmid,
-    "--hostname",
-    options.name,
-    "--description",
-    (await readFromUrl(new URL("template/summary.md", import.meta.url)))
-      .replaceAll("${APP_NAME}", options.name)
-      .replaceAll("${DOCKER_CT_TEMPLATE_FILENAME}", `${options.templateVmid}`),
-  ];
-  await run(cloneCmd).catch(() =>
-    run([
-      ...cloneCmd,
-      "--full",
-      1,
-    ])
-  );
-  const storageName = (await options.storage()).name;
-  const appdataDir = await getAppdataDir({ name: storageName }, options);
-  await Deno.mkdir(appdataDir, { recursive: true });
-  await Deno.writeTextFile(
-    appdataDir + "/docker-compose.yml",
-    await readFromUrl(new URL("template/docker-compose.yml", import.meta.url)),
-  );
-  await run(["chown", "-R", `${100_000}:${100_000}`, appdataDir]);
-  await run([
-    "pct",
-    "set",
-    vmid,
-    "--mp0",
-    `${appdataDir},mp=/appdata,backup=1`,
-  ]);
-  return { vmid, appdataDir };
+export function parsePasswdRow(row: string): PasswdRow {
+  const [
+    username,
+    password,
+    uid,
+    gid,
+    gecos,
+    home,
+    shell,
+  ] = row.split(":");
+  return {
+    username,
+    password,
+    uid: Number(uid),
+    gid: Number(gid),
+    gecos,
+    home,
+    shell,
+  };
 }
 
-export async function ensureExistsCtTemplate(
-  options: CtTemplateOptions,
-): Promise<VMID> {
-  const template: PctEntry | undefined = await getCtTemplate(options.name);
-  if (!template) {
-    return await createCtTemplate(options);
+export async function getPasswdRowOfLocalUser(
+  username: string,
+): Promise<PasswdRow> {
+  const passwd = await run(["getent", "passwd", username]);
+  return parsePasswdRow(passwd);
+}
+
+export async function resolveSshKey(sshKey: SshKey): Promise<SshKeyRaw> {
+  if (isPrefixedUsername(sshKey)) {
+    const [prefix, username] = sshKey.split(":");
+    if (prefix === "gh") {
+      return await (await fetch(`https://github.com/${username}.keys`))
+        .text() as SshKeyRaw;
+    }
+    if (prefix === "local") {
+      const homeDir = (await getPasswdRowOfLocalUser(username)).home;
+      return await run([
+        "sh",
+        "-c",
+        `cat "${homeDir}"/.ssh/*.pub`,
+      ]) as SshKeyRaw;
+    }
   }
-  return template.vmid;
+  if (isSshKeyRaw(sshKey)) {
+    return sshKey;
+  }
+  throw new Error(`Invalid ssh key: ${sshKey}`);
 }
 
-export async function ensureExistsTemplate(
-  templateStorage: () => Promise<StorageRow>,
-  filename: string,
-): Promise<string> {
-  await run("pveam update");
-  const storageName = (await templateStorage()).name;
+export async function resolveSshKeys(sshKeyArg: MultiArgument<SshKey>): Promise<
+  SshKeyRaw[]
+> {
+  const sshKeys = await resolveMultiArgument(sshKeyArg);
+  return await Promise.all(sshKeys.map(resolveSshKey));
+}
+
+type AppContainerCreateOptions = {
+  name: string;
+  cidr: string;
+  sshKey: MultiArgument<SshKey>;
+};
+
+export async function createAppContainer(
+  options: AppContainerCreateOptions,
+): Promise<{ appdataDir: string }> {
+  const installScript = await readInstallScript("alpine-318");
+  const runcmd = [installScript];
+  // const appdataDir = await getAppdataDir(options);
+  // await Deno.mkdir(appdataDir, { recursive: true });
+  // await Deno.writeTextFile(
+  //   appdataDir + "/docker-compose.yml",
+  //   await readFromUrl(new URL("template/docker-compose.yml", import.meta.url)),
+  // );
+  // await run(["chown", "-R", `${100_000}:${100_000}`, appdataDir]);
+
+  const image = "images:alpine/3.19/cloud";
+  const defaultGateway = new IPCIDR(options.cidr).toArray({
+    from: 1,
+    limit: 1,
+  }).at(0);
+  const nameserver = defaultGateway;
+  const size = "10GiB";
+
+  const sshKeys: SshKey[] = await resolveMultiArgument(
+    options.sshKey as MultiArgument<SshKey>,
+  );
+  const sshKeysRaw: SshKeyRaw[] = await Promise.all(sshKeys.map(resolveSshKey));
+
+  const vendorConfig = {
+    ...(sshKeysRaw.length === 0 ? {} : {
+      write_files: [
+        {
+          path: "/root/.ssh/authorized_keys",
+          content: sshKeysRaw.join("\n"),
+          permissions: "0600",
+        },
+      ],
+    }),
+    manage_resolv_conf: true,
+    resolv_conf: {
+      nameservers: [nameserver],
+    },
+    packages: [
+      "bash",
+      "curl",
+    ],
+    runcmd,
+    power_state: {
+      mode: "reboot",
+      timeout: 30,
+    },
+  };
+
+  const userConfig = {};
+
+  const networkConfigYaml = options.cidr === "dhcp" ? "DHCP on eth0" : yaml({
+    network: {
+      version: 2,
+      ethernets: {
+        eth0: {
+          addresses: [options.cidr],
+          routes: [{ to: "0.0.0.0/0", via: defaultGateway }],
+          nameservers: { addresses: [nameserver] },
+        },
+      },
+    },
+  });
+
   await run([
-    "pveam",
-    "download",
-    storageName,
-    filename,
+    "incus",
+    "create",
+    "--no-profiles",
+    "--storage=default",
+    image,
+    options.name,
+  ], {
+    stdin: yaml({
+      description: "By incus-app-container",
+      devices: {
+        eth0: {
+          name: "eth0",
+          nictype: "bridged",
+          parent: "br0",
+          type: "nic",
+        },
+        root: {
+          path: "/",
+          pool: "default",
+        },
+      },
+      config: {
+        "security.nesting": "true",
+        "cloud-init.user-data": "#cloud-config\n" + yaml(userConfig),
+        "cloud-init.vendor-data": "#cloud-config\n" + yaml(vendorConfig),
+        "cloud-init.network-config": networkConfigYaml,
+      },
+    }),
+  });
+  await run([
+    "incus",
+    "config",
+    "device",
+    "set",
+    "via-script",
+    "root",
+    `size=${size}`,
   ]);
-  return `${storageName}:${CONTENT_CT_TEMPLATE}/${filename}`;
+
+  return {
+    appdataDir:
+      `created container ${options.name} successfully, but no appdata dir yet.`,
+  };
 }
