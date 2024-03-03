59355fae17ea411ebd40a1ed1770af6621ae47a4
Hugo Josefson <hugo@josefson.org>
2024-03-03 17:14:29 +0100

    chore: delete proxmox specific files

--- usr_local_bin/incus-app-container-files/run.ts
+++
@@ -1,9 +0,0 @@
-import { defaultRunOptions, run, RunOptions, SimpleValue } from "./deps.ts";
-import { parseColumns } from "./parse-columns.ts";
-
-export async function columnRun<T>(
-  command: string | SimpleValue[],
-  options: RunOptions = defaultRunOptions,
-): Promise<T[]> {
-  return parseColumns<T>(await run(command, options));
-}
