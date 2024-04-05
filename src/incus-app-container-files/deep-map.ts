3897d4675e8e486ac70017e4bad2dd98a8b74f8c
Hugo Josefson <hugo@josefson.org>
2024-04-05 19:51:52 +0200

    chore: delete unused create-deep-map-values

--- src/incus-app-container-files/deep-map.ts
+++ src/incus-app-container-files/deep-map.ts
@@ -1,19 +1,3 @@
-export function createDeepMapValues(fn: <T>(t: T) => T) {
-  return function deepMapValues<X>(x: X): X {
-    if (Array.isArray(x)) {
-      return x.map(deepMapValues) as X;
-    }
-
-    if (typeof x === "object" && x !== null) {
-      return Object.fromEntries(
-        Object.entries(x).map(([key, value]) => [key, deepMapValues(value)]),
-      ) as X;
-    }
-
-    return fn(x);
-  };
-}
-
 export function createDeepMapKeys(fn: (s: string) => string) {
   return function deepMapKeys<X>(x: X): X {
     if (Array.isArray(x)) {
