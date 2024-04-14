2c920c700a8c9dd86c2f86caadd0ec5a698d73fb
Hugo Josefson <hugo@josefson.org>
2024-04-14 19:13:41 +0200

    chore: bump deps

--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,2 @@
-export { describe, it } from "https://deno.land/std@0.221.0/testing/bdd.ts";
-export { assertEquals } from "https://deno.land/std@0.221.0/assert/assert_equals.ts";
+export { describe, it } from "https://deno.land/std@0.222.1/testing/bdd.ts";
+export { assertEquals } from "https://deno.land/std@0.222.1/assert/assert_equals.ts";
